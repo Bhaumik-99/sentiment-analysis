4fde4c5b5f794a3d2c24d43bc3be7365f4f49533
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2025-10-08 00:00:13 +0530

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,7 +1,7 @@
  import pandas as pd 
 import numpy as np 
 import re
-import pickle
+import pickle 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
